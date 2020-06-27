fa9fd265e3b73bc0548198dacc33a436fcb1ab53
Serhii Kis <4irius@gmail.com>
2020-06-27 14:21:58 +0300

    Adding moving background and hardcoded Rank component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,87 @@ import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 import 'tachyons';
+import Particles from 'react-particles-js';
+
+const particlesOptions = {
+  interactivity: {
+    detectsOn: "canvas",
+    events: {
+      onClick: {
+        enable: true,
+        mode: "push"
+      },
+      onHover: {
+        enable: false,
+        mode: "repulse"
+      },
+      resize: true
+    },
+    modes: {
+      bubble: {
+        distance: 400,
+        duration: 2,
+        opacity: 0.8,
+        size: 40,
+        speed: 1
+      },
+      push: {
+        quantity: 4
+      },
+      repulse: {
+        distance: 200,
+        duration: 0.4
+      }
+    }
+  },
+  particles: {
+    color: {
+      value: "#ffffff"
+    },
+    
+    collisions: {
+      enable: true
+    },
+    move: {
+      direction: "none",
+      enable: true,
+      outMode: "bounce",
+      random: false,
+      speed: 3,
+      straight: false
+    },
+    number: {
+      density: {
+        enable: true,
+        value_area: 800
+      },
+      value: 80
+    },
+    opacity: {
+      value: 0.5
+    },
+    shape: {
+      type: "circle"
+    },
+    size: {
+      random: true,
+      value: 5
+    }
+  }
+
+}
 
 function App() {
   return (
     <div className="App">
+      <Particles className="particles"
+      params={particlesOptions}
+      />
      <Navigation />
      <Logo />
+     <Rank />
      <ImageLinkForm />
   
     </div>
